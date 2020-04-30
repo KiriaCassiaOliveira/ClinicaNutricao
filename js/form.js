7aa308757e9b69c9825e34e3476e723b954f6b35
Kiria Oliveira <kiriacsoliveira@gmail.com>
2020-04-30 17:51:19 -0300

    Correção Linha de Código Duplicada

--- js/form.js
+++ js/form.js
@@ -15,7 +15,6 @@ botaoAdicionar.addEventListener("click", function (event) {
 
   var tabela = document.querySelector("#tabela-pacientes");
 
-  tabela.appendChild(pacienteTr);
   adicionaPacienteNaTabela(paciente);
   form.reset();
 
